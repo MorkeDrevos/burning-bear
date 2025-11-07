a6b5b5b677920a90a9ce62eb1852ff8a5c0ff70a
Mørke Drevos <drtseo@proton.me>
2025-11-07 11:49:06 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -486,8 +486,7 @@ return (
           </div>
         </div>
       </section>
-      
-{/** ===== Campfire Bonus (broadcast only) ===== */}
+
 {broadcast.on && Boolean(broadcast.params.get('reward')) && (
   <section className="w-full px-4 sm:px-6 lg:px-8 mt-4">
     <div className="mx-auto max-w-6xl">
